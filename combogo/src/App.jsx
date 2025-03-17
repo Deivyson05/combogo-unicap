404c306f399afb356b86c27d9a4d490e432a1701
DeivRicardoSS <deivysonrssantos@outlook.com>
2025-03-17 18:25:25 -0300

    home, slide e sobre
    Co-authored-by: Vinicius Miranda <viniciussmiranda@users.noreply.github.com>

--- combogo/src/App.jsx
+++ combogo/src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 // import './App.css'
 import {Header} from './components/Header';
+import {Home} from './sections/Home';
+import {About} from './sections/About';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,8 @@ function App() {
   return (
     <>
       <Header/>
+      <Home/>
+      <About />
     </>
   )
 }
